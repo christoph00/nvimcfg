e41e0a142a8ca27aee3acfde034f0844efe3869a
Christoph <christoph00@users.noreply.github.com>
2024-09-18 07:34:47 +0200

    Update nix.lua

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -11,8 +11,8 @@ local root_files = {
 }
 
 vim.lsp.start {
-  name = 'nil_ls',
-  cmd = { 'nil' },
+  name = 'nixd_ls',
+  cmd = { 'nixd' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
 }
